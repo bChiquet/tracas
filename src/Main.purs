555f5c33a7b67634abdbb9395a71f111a4f84412
bec <tracas@binarytrove.com>
2021-07-13 13:25:47 +0200

    improve the cookie/session generation

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,6 @@
 module Main where
 
-import Prelude (Unit, pure, (#), bind, discard, unit, show)
+import Prelude (Unit, pure, (#), bind, discard, unit, (-), (<), (*))
 import Control.Monad ((>>=), void)
 import Control.Monad.Rec.Class (forever)
 import Control.Monad.Loops (whileJust)
@@ -9,19 +9,25 @@ import Effect.Console (log)
 import Effect.Class (liftEffect)
 import Effect.Random (randomInt)
 import Data.Functor (map)
+import Data.Array ((:), length)
 import Data.String.Utils (words)
+import Data.String.CodeUnits (charAt, fromCharArray)
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Either (Either(..))
 import Data.Number as Number
-import Data.Int (floor)
+import Data.Int as Int
+import Math (floor)
+import Data.EuclideanRing (div)
 import Data.Traversable (traverse)
 import Data.Time.Duration (Milliseconds(..))
 import Data.DateTime.Instant (unInstant)
 import Data.Monoid ((<>))
 import Data.Argonaut.Encode (encodeJson)
 import Browser.Cookie (getCookie, setCookie)
-import Browser.Cookies.Data (Cookie(..), SetCookie(..))
+import Browser.Cookies.Data
+  (Cookie(..), SetCookie(..), CookieOpts(..), SameSite(..))
 import Effect.Now (now)
+import Data.Unfoldable (replicateA)
 
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.DOM.Internal.Types (Element)
@@ -71,12 +77,55 @@ getAttr attr mbNode = case mbNode of
 withDefault :: forall a. a -> Maybe a -> a
 withDefault = fromMaybe
 
+
+-- | gets successive remainders of an int divided by 64 until it reaches 0.
+-- | Operates on `Number` because `Int` is limited to 2^32
+-- | Precision is neglected, since this is used for a hash.
+successiveModulos :: Number -> Array Int
+successiveModulos n = 
+  let intDiv m divider = floor (div m divider)
+      mod m divider = m - (intDiv m divider * divider) 
+      allMods n_ modList = case intDiv n_ 64.0 of
+        epsilon | epsilon < 1.0   -> mod n_ 64.0 : modList
+        rest                      -> allMods rest ((mod n_ 64.0) : modList) 
+   in map Int.floor (allMods n [])
+
+-- | create an identifier based on creation time and random input
 makeIdentifier :: Effect String
 makeIdentifier = do
+  let charSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#" 
   (Milliseconds posix) <- now # map unInstant
-  randomValue <- randomInt 0 10000000
-  pure (show (floor posix) <> show randomValue)
+  let prefix = successiveModulos posix
+  suffix <- replicateA (16 - length prefix) (randomInt 0 63)
+  map (\n -> charAt n charSet # withDefault '?') (prefix <> suffix)
+    # fromCharArray
+    # pure
 
+-- | Get cookie from key.
+-- | If no cookie is retrieved, store one with defaultValue.
+getOrCreateCookie :: String -> Identifier -> Effect Identifier
+getOrCreateCookie cookieKey defaultValue = do
+  mbCookie <- getCookie cookieKey
+  cookieValue <- case mbCookie of
+    Just (Cookie content) ->
+      pure content.value
+    Nothing     -> do
+      setCookie 
+        (SetCookie 
+          {cookie: (Cookie {key: cookieKey, value: defaultValue})
+          , opts: Just (CookieOpts 
+             { domain : Nothing
+             , expires : Nothing
+             , httpOnly : false
+             , maxAge : Nothing
+             , path : Nothing
+             , samesite : Just Lax
+             , secure : true
+             })
+          }
+        )
+      pure defaultValue
+  pure cookieValue
 
 buildConfig :: Effect (Maybe Config)
 buildConfig = do
@@ -97,22 +146,14 @@ buildConfig = do
               # map (map words)
 
   session <- makeIdentifier
-  mbCookie  <- getCookie "tracas-cookie"
-  cookieValue <- case mbCookie of
-    Just (Cookie content) ->
-      pure content.value
-    Nothing     -> do
-      setCookie (SetCookie 
-                  {cookie: (Cookie {key: "tracas-cookie", value: session})
-                  , opts: Nothing})
-      pure session
+  cookie <- getOrCreateCookie "tracas-cookie" session
 
   case [mbAppName, mbServer] of
     [Just appName, Just server] -> pure (Just 
       { appName        : appName
       , server         : server
       , session        : session 
-      , cookie         : cookieValue
+      , cookie         : cookie
       , dataSet        : dataSet    # withDefault "undefined"
       , heartBeatEvery : hBeatEvery # withDefault (Milliseconds 200.0)
       , phoneHomeEvery : phoneEvery # withDefault (Milliseconds 5000.0)
